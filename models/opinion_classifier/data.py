ef37ad3e829d878133b6565b4f645b378260bac4
Viktor Kovryzhkin <vik.kovrizhkin@gmail.com>
2018-05-20 16:53:08 +0300

    ulmfit classifier based on language model

--- models/opinion_classifier/data.py
+++ models/opinion_classifier/data.py
@@ -1,4 +1,5 @@
 import re
+import html
 import multiprocessing as mp
 from functools import partial
 
@@ -10,6 +11,8 @@ import gensim
 
 from sklearn.model_selection import train_test_split
 from sklearn.utils import resample, shuffle
+
+from fastai.text import *
 
 
 class VocabularyProcessor(object):
@@ -65,9 +68,76 @@ class Word2VecEmbeddingsLoader(object):
         return self._embedding_matrix
 
 
-def get_data(files, feature_col, target_col):
-    data = pd.concat([pd.read_csv(x) for x in files])
-    data = down_sample(data)
+class UKTokenizer():
+    def __init__(self):
+        self.re_br = re.compile(r'<\s*br\s*/?>', re.IGNORECASE)
+
+    def sub_br(self,x):
+        return self.re_br.sub("\n", x)
+
+    def tokenize(self, x):
+        return tokenize_uk.tokenize_words(self.sub_br(x))
+
+    re_rep = re.compile(r'(\S)(\1{3,})')
+    re_word_rep = re.compile(r'(\b\w+\W+)(\1{3,})')
+
+    @staticmethod
+    def replace_rep(m):
+        TK_REP = 'tk_rep'
+        c,cc = m.groups()
+        return f' {TK_REP} {len(cc)+1} {c} '
+
+    @staticmethod
+    def replace_wrep(m):
+        TK_WREP = 'tk_wrep'
+        c,cc = m.groups()
+        return f' {TK_WREP} {len(cc.split())+1} {c} '
+
+    @staticmethod
+    def do_caps(ss):
+        TOK_UP,TOK_SENT,TOK_MIX = ' t_up ',' t_st ',' t_mx '
+        res = []
+        prev='.'
+        re_word = re.compile('\w')
+        re_nonsp = re.compile('\S')
+        for s in re.findall(r'\w+|\W+', ss):
+            res += ([TOK_UP,s.lower()] if (s.isupper() and (len(s)>2))
+    #                 else [TOK_SENT,s.lower()] if (s.istitle() and re_word.search(prev))
+                    else [s.lower()])
+    #         if re_nonsp.search(s): prev = s
+        return ''.join(res)
+
+    def proc_text(self, s):
+        s = self.re_rep.sub(UKTokenizer.replace_rep, s)
+        s = self.re_word_rep.sub(UKTokenizer.replace_wrep, s)
+        s = UKTokenizer.do_caps(s)
+        s = re.sub(r'([/#])', r' \1 ', s)
+        s = re.sub(' {2,}', ' ', s)
+        return self.tokenize(s)
+
+    @staticmethod
+    def proc_all(ss):
+        tok = UKTokenizer()
+        return [tok.proc_text(s) for s in ss]
+
+    @staticmethod
+    def proc_all_mp(ss):
+        ncpus = num_cpus()//2
+        with ProcessPoolExecutor(ncpus) as e:
+            return sum(e.map(UKTokenizer.proc_all, ss), [])
+
+
+def get_data(files, feature_col, target_col, downsample=False, escapechar=None, convert=False):
+    data = pd.concat([pd.read_csv(x, escapechar=escapechar) for x in files])
+
+    if convert:
+        o = data[data[target_col] == '0']
+        s = data[data[target_col] == '1']
+        data = pd.concat([o, s])
+        data[target_col] = data[target_col].astype(int)
+
+    if downsample:
+        data = down_sample(data)
 
     X = data[feature_col].apply(tokenize_uk.tokenize_words)
     y = data[target_col]
@@ -104,5 +174,43 @@ def delete_stop_words(words, stop_words):
             result.append(w)
     return result
 
-def tokenizer(x): 
-    return x
+
+def clean_data(text):
+    reg = re.compile("""[\"#$%&*\-+/:;<=>@^`~…\\(\\)⟨⟩{}\[\|\]‒–—―«»“”‘’№]""")
+    result = text.apply(lambda sent: [re.sub(reg, '', x) for x in sent])
+    result = result.apply(lambda sent: [x for x in sent if x.strip()])
+
+    result = result.apply(lambda sent: [x.lower() for x in sent])
+
+    return result
+
+
+re1 = re.compile(r'  +')
+
+
+def fixup(x):
+    x = x.replace('#39;', "'").replace('amp;', '&').replace('#146;', "'").replace(
+        'nbsp;', ' ').replace('#36;', '$').replace('\\n', "\n").replace('quot;', "'").replace(
+        '<br />', "\n").replace('\\"', '"').replace('<unk>','u_n').replace(' @.@ ','.').replace(
+        ' @-@ ','-').replace('\\', ' \\ ')
+    return re1.sub(' ', html.unescape(x))
+
+
+def get_texts(df, n_lbls=1):
+    labels = df.iloc[:,range(n_lbls)].values.astype(np.int64)
+    texts = f'\n{BOS} {FLD} 1 ' + df[n_lbls].astype(str)
+    for i in range(n_lbls+1, len(df.columns)): texts += f' {FLD} {i-n_lbls} ' + df[i].astype(str)
+    texts = texts.apply(fixup).values.astype(str)
+
+    tok = UKTokenizer().proc_all_mp(partition_by_cores(texts))
+    return tok, list(labels)
+
+
+def get_all(df, n_lbls):
+    tok, labels = [], []
+    for i, r in enumerate(df):
+        print(i)
+        tok_, labels_ = get_texts(r, n_lbls)
+        tok += tok_;
+        labels += labels_
+    return tok, labels
---
+++ models/opinion_classifier/ulmfit_classifier.py
@@ -0,0 +1,112 @@
+import argparse
+import pickle
+import collections
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from fastai.text import *
+
+from data import *
+
+
+def main(args):
+    df = pd.read_csv(args.input_file, skiprows=1, header=None, chunksize=args.chunksize)
+
+    texts, labels = get_all(df, 1)
+
+    text_train, text_test, labels_train, labels_test = train_test_split(texts, labels, test_size=0.2,
+                                                                        random_state=1234, stratify=labels)
+
+    with open(args.itos_path, 'rb') as f:
+        itos = pickle.load(f)
+    stoi = collections.defaultdict(lambda: 0, {v: k for k, v in enumerate(itos)})
+
+    train_clas = np.array([[stoi[o] for o in p] for p in text_train])
+    val_clas = np.array([[stoi[o] for o in p] for p in text_test])
+    trn_labels = np.squeeze(np.array(labels_train))
+    val_labels = np.squeeze(np.array(labels_test))
+
+    bptt, em_sz, nh, nl = 70, 400, 1150, 3
+    vs = len(itos)
+
+    # select our optimizer
+    # also pick a batch size as big as you can that doesn't run out of memory
+    opt_fn = partial(optim.Adam, betas=(0.8, 0.99))
+    bs = 48
+
+    min_lbl = trn_labels.min()
+    trn_labels -= min_lbl
+    val_labels -= min_lbl
+    c = int(trn_labels.max()) + 1
+
+    trn_ds = TextDataset(train_clas, trn_labels)
+    val_ds = TextDataset(val_clas, val_labels)
+
+    # sort the docs based on size.
+    # validation will be explicitly short -> long
+    # training, which sorts loosely
+    trn_samp = SortishSampler(train_clas, key=lambda x: len(train_clas[x]), bs=bs // 2)
+    val_samp = SortSampler(val_clas, key=lambda x: len(val_clas[x]))
+
+    # then we create our dataloaders as before but with a [sampler] parameter
+    trn_dl = DataLoader(trn_ds, bs // 2, transpose=True, num_workers=1, pad_idx=1, sampler=trn_samp)
+    val_dl = DataLoader(val_ds, bs, transpose=True, num_workers=1, pad_idx=1, sampler=val_samp)
+    md = ModelData(args.lm_model_path, trn_dl, val_dl)
+
+    # setup our dropout rates
+    dps = np.array([0.4, 0.5, 0.05, 0.3, 0.4]) * 0.5
+    m = get_rnn_classifer(bptt, 20 * 70, c, vs, emb_sz=em_sz, n_hid=nh, n_layers=nl, pad_token=1,
+                          layers=[em_sz * 3, 50, c], drops=[dps[4], 0.1],
+                          dropouti=dps[0], wdrop=dps[1], dropoute=dps[2], dropouth=dps[3])
+
+    opt_fn = partial(optim.Adam, betas=(0.7, 0.99))
+
+    # define our RNN learner
+    learn = RNN_Learner(md, TextModel(to_gpu(m)), opt_fn=opt_fn)
+    learn.reg_fn = partial(seq2seq_reg, alpha=2, beta=1)
+    learn.clip = 25.
+    learn.metrics = [accuracy]
+
+    # set our learning rate
+    # we will use discriminative learning rates for different layers
+    lr = 3e-3
+    lrm = 2.6
+    lrs = np.array([lr / (lrm ** 4), lr / (lrm ** 3), lr / (lrm ** 2), lr / lrm, lr])
+
+    # Now we load our language model from before
+    # but freeze everything except the last layer
+    lrs = np.array([1e-4, 1e-4, 1e-4, 1e-3, 1e-2])
+    wd = 1e-7
+    wd = 0
+    learn.load_encoder(args.lm_model_name)
+    learn.freeze_to(-1)
+
+    learn.lr_find(lrs / 1000)
+
+    # train last layer
+    learn.fit(lrs, 1, wds=wd, cycle_len=1, use_clr=(8, 3))
+    learn.save('clas_0')
+
+    # train two last layers
+    learn.load('clas_0')
+    learn.freeze_to(-2)
+    learn.fit(lrs, 1, wds=wd, cycle_len=1, use_clr=(8, 3))
+    learn.save('clas_1')
+
+    learn.load('clas_1')
+    learn.unfreeze()
+    learn.fit(lrs, 1, wds=wd, cycle_len=5, use_clr=(32, 10), best_save_name='clas_best', cycle_save_name='clas_cycle')
+    learn.save('clas_final')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='ULM fit retrained classifier')
+    parser.add_argument('--input_file', dest='input_file', help='Input CSV file')
+    parser.add_argument('--itos_path', dest='itos_path', help='Index to word pickle file')
+    parser.add_argument('--lm_model_path', dest='lm_model_path', help='Path to trained ULMfit model')
+    parser.add_argument('--lm_model_name', dest='lm_model_name', help='Name of trained ULMfit model')
+    parser.add_argument('--chunksize', dest='chunksize', default=2000, type=int, help='Pandas chunk size')
+
+    args = parser.parse_args()
+    main(args)
